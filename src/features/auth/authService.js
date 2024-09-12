a5e2b94939ff35431890a053a3e6f7f356047e33
sachinmandloi06 <sachinmandloi06@gmail.com>
2024-09-12 18:21:41 +0530

    api_updated

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
+const API_URL = 'https://authenticationeskills.vercel.app/api/user'
+
 export const register = async (formData) => {
-  const response = await axios.post("/api/user/register", formData);
+  const response = await axios.post(API_URL + "/register", formData);
   localStorage.setItem("user", JSON.stringify(response.data));
   return response.data;
 };
 
 export const login = async (formData) => {
-  const response = await axios.post("/api/user/login", formData);
+  const response = await axios.post(API_URL + "/login", formData);
   localStorage.setItem("user", JSON.stringify(response.data));
   return response.data;
 };
